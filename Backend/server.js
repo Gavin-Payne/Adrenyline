5b3a60d186666c5f72e91a2f03721e3876af3e5a
Gavin Payne <g.payne2189@gmail.com>
2025-06-04 17:21:22 -0600

    cleaned url

--- Backend/server.js
+++ Backend/server.js
@@ -22,7 +22,7 @@ const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
   'http://localhost:3000',
-  'https://imperium-6msr.onrender.com/api'
+  'https://imperium-6msr.onrender.com'
 ];
 app.use(cors({
   origin: function(origin, callback) {
